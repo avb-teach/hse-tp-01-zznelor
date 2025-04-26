d12aaf3b4d1bb43a4951b4d650ed347b8e47bffc
zznelor <zsneporozhnev@edu.hse.ru>
2025-04-26 16:49:36 +0000

    Добавил скрипт для проверки на одинаковые названия

--- collect_files.sh
+++ collect_files.sh
@@ -13,7 +13,22 @@ mkdir -p "$output_dir"
 echo "Input Directory: $input_dir"
 echo "Output Directory: $output_dir"
 
-for file in $(find "$input_dir" -type f); do  #https://losst.pro/komanda-find-v-linux файндом отсюда пользоваться смотрел как
+for file in $(find "$input_dir" -type f); do #https://losst.pro/komanda-find-v-linux файндом отсюда пользоваться смотрел как. Решает все проблемы этого мира (файнд, не сайт)
   filename=$(basename "$file")
-  cp "$file" "$output_dir/$filename"
+  destination="$output_dir/$filename"
+
+  if [ -e "$destination" ]; then
+    name="${filename%.*}"
+    ext="${filename##*.}"
+    counter=1
+
+    while [ -e "$output_dir/${name}_${counter}.${ext}" ]; do
+      counter=$((counter + 1))
+    done
+
+    cp "$file" "$output_dir/${name}_${counter}.${ext}"
+  else
+    cp "$file" "$destination"
+  fi
 done
+
